bbc388281824509945d60f0e074fe24db3f8c533
felipemotavieira <felipemtvieira@gmail.com>
2023-02-12 22:34:09 -0300

    fixing url

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 const api = axios.create({
-    baseURL: 'https://teste-supera-back.onrender.com/api',
-    timeout: 10000
+    baseURL: 'https://teste-supera-back.onrender.com/api/',
+    timeout: 1000000
 })
 
 export default api
